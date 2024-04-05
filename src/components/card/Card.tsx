7bb2e414b706822376abe0b155c3004726bbfebb
SergFediaev <sergfediaev@gmail.com>
2024-04-05 21:42:51 +0300

    Рефакторинг карточек.

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -21,37 +21,30 @@ type CardPropsType = {
 export const Card = memo(({id, title, text, tags, likes, dislikes, comments, favorite, ...rest}: CardPropsType) => {
     const [open, setOpen] = useState<boolean>(rest.open || true)
     const parent = useRef(null)
-
+    const dispatch = useDispatch()
     useEffect(() => {
         parent.current && autoAnimate(parent.current)
     }, [parent])
 
-    const dispatch = useDispatch()
-
     const like = () => dispatch(likeCard(id))
-
     const copy = () => navigator.clipboard.writeText(`${title}\n\n${text}`)
 
     return <li className={s.card} ref={parent}>
         <h3>{title}</h3>
-        <p>Tags: {tags.length > 0 ? tags : STRINGS.UNCATEGORIZED}</p>
+        <p>{STRINGS.TAGS}: {tags.length > 0 ? tags : STRINGS.UNCATEGORIZED}</p>
         {open && <main>
             <p>{text}</p>
         </main>}
         <div className={s.menu}>
             <ButtonIcon onClick={like}
                         title={TITLES.LIKE}>{EMOJIS.LIKE} {likes}</ButtonIcon>
-            <ButtonIcon onClick={() => {
-            }}
+            <ButtonIcon onClick={() => {}}
                         title={TITLES.DISLIKE}>{EMOJIS.DISLIKE} {dislikes}</ButtonIcon>
-            <ButtonIcon onClick={() => {
-            }}
+            <ButtonIcon onClick={() => {}}
                         title={TITLES.COMMENTS}>{EMOJIS.COMMENTS} {comments.length}</ButtonIcon>
-            <ButtonIcon onClick={() => {
-            }}
+            <ButtonIcon onClick={() => {}}
                         title={TITLES.FAVORITE}>{EMOJIS.FAVORITE}</ButtonIcon>
-            <ButtonIcon onClick={() => {
-            }}
+            <ButtonIcon onClick={() => {}}
                         title={TITLES.VIEW}>{EMOJIS.VIEW}</ButtonIcon>
             <ButtonIcon onClick={copy}
                         title={TITLES.COPY}>{EMOJIS.COPY}</ButtonIcon>
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const STRINGS = {
     EMPTY: '',
+    TAGS: 'Tags',
     ALL: 'ALL',
     UNCATEGORIZED: 'Uncategorized',
     REACT: 'React',
