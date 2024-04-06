b1107fd08fe02d3bd3040e568c9bc1628674bd03
SergFediaev <sergfediaev@gmail.com>
2024-04-06 22:28:03 +0300

    HashRouter маршрутизация.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals'
 import {App} from './components/app/App'
 import {Provider} from 'react-redux'
 import {store} from './store/store'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
@@ -14,9 +14,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>,
 )
